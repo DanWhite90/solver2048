567eafecd22b221485793421e7acd95a92cb1acf
Daniele Bianchin <bianchin.daniele.b@gmail.com>
2020-06-05 22:25:37 +0100

    fix controls layout

--- src/components/game/GameControls.js
+++ src/components/game/GameControls.js
@@ -6,23 +6,37 @@ import {faArrowUp, faArrowLeft, faArrowRight, faArrowDown} from '@fortawesome/fr
 const GameControls = () => {
   return (
     <Container className="controls">
-      <Row>
-        <Col></Col>
-        <Col as={Button} variant="primary" className="rounded-circle"><FontAwesomeIcon icon={faArrowUp} size="lg"/></Col>
-        <Col></Col>
+      <Row className="justify-content-center">
+        <Col xs={4} as={Button} variant="primary" className="rounded-circle"><FontAwesomeIcon icon={faArrowUp} size="lg"/></Col>
       </Row>
-      <Row>
-        <Col as={Button} variant="primary" className="rounded-circle"><FontAwesomeIcon icon={faArrowLeft} size="lg"/></Col>
-        <Col></Col>
-        <Col as={Button} variant="primary" className="rounded-circle"><FontAwesomeIcon icon={faArrowRight} size="lg"/></Col>
+      <Row className="justify-content-between">
+        <Col xs={4} as={Button} variant="primary" className="rounded-circle"><FontAwesomeIcon icon={faArrowLeft} size="lg"/></Col>
+        <Col xs={4} as={Button} variant="primary" className="rounded-circle"><FontAwesomeIcon icon={faArrowRight} size="lg"/></Col>
       </Row>
-      <Row>
-        <Col></Col>
-        <Col as={Button} variant="primary" className="rounded-circle"><FontAwesomeIcon icon={faArrowDown} size="lg"/></Col>
-        <Col></Col>
+      <Row className="justify-content-center">
+        <Col xs={4} as={Button} variant="primary" className="rounded-circle"><FontAwesomeIcon icon={faArrowDown} size="lg"/></Col>
       </Row>
     </Container>
   );
+  // return (
+  //   <Container className="controls">
+  //     <Row>
+  //       <Col></Col>
+  //       <Col as={Button} variant="primary" className="rounded-circle"><FontAwesomeIcon icon={faArrowUp} size="lg"/></Col>
+  //       <Col></Col>
+  //     </Row>
+  //     <Row>
+  //       <Col as={Button} variant="primary" className="rounded-circle"><FontAwesomeIcon icon={faArrowLeft} size="lg"/></Col>
+  //       <Col></Col>
+  //       <Col as={Button} variant="primary" className="rounded-circle"><FontAwesomeIcon icon={faArrowRight} size="lg"/></Col>
+  //     </Row>
+  //     <Row>
+  //       <Col></Col>
+  //       <Col as={Button} variant="primary" className="rounded-circle"><FontAwesomeIcon icon={faArrowDown} size="lg"/></Col>
+  //       <Col></Col>
+  //     </Row>
+  //   </Container>
+  // );
 }
 
 export default GameControls;
