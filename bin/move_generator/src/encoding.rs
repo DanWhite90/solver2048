a8f66097ef762a2a24f0a72c893d27bf1d8b027d
Daniele Bianchin <bianchin.daniele.b@gmail.com>
2020-07-18 03:17:48 +0100

    add rust decode

--- bin/move_generator/src/encoding.rs
+++ bin/move_generator/src/encoding.rs
@@ -13,8 +13,24 @@ pub fn encode_row(row: &[u32]) -> u32 {
   num
 }
 
+pub fn decode_row(mut num: u32) -> [u32; 4] {
+  let mut row = [0, 0, 0, 0];
+  let mut tile;
+
+  for count in 0..row.len() {
+    tile = ((num % (ENCODING_BITS as f64).exp2() as u32) as f64).exp2() as u32;
+    num >>= ENCODING_BITS;
+    if tile > 1 {
+      row[count] = tile;
+    }
+  }
+  row
+}
+
 #[cfg(test)]
 mod tests {
+
+  // testing for encode_row()
   #[test]
   fn correct_zero_encoding() {
     let row = [0, 0, 0, 0];
@@ -31,5 +47,21 @@ mod tests {
   fn correct_large_encoding() {
     let row = [65536, 65536, 65536, 65536];
     assert_eq!(super::encode_row(&row), 541200);
+  }
+
+  // testing for decode_row()
+  #[test]
+  fn correct_zero_decoding() {
+    assert_eq!(super::decode_row(0), [0, 0, 0, 0]);
+  }
+  
+  #[test]
+  fn correct_random_decoding() {
+    assert_eq!(super::decode_row(1091), [8, 4, 2, 0]);
+  }
+  
+  #[test]
+  fn correct_large_decoding() {
+    assert_eq!(super::decode_row(541200), [65536, 65536, 65536, 65536]);
   }
 }
--- bin/move_generator/src/main.rs
+++ bin/move_generator/src/main.rs
@@ -4,5 +4,8 @@ fn main() {
   println!("This is the moves generator!");
 
   let row = [8,4,2,0];
-  println!("calling encode(): {}", encoding::encode_row(&row));
+  println!("calling encode({:?}): {}", row, encoding::encode_row(&row));
+
+  let num = 541200;
+  println!("calling decode({}): {:?}", num, encoding::decode_row(541200));
 }
