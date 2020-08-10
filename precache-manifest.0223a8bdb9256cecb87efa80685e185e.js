ccabb0b5220a68fe20a58c7860c2defd39721115
Daniele Bianchin <bianchin.daniele.b@gmail.com>
2020-08-10 20:38:03 +0100

    Updates

---
+++ precache-manifest.0223a8bdb9256cecb87efa80685e185e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9f360bd49c1df636702ce78988a5666",
+    "url": "/solver2048/index.html"
+  },
+  {
+    "revision": "3159f02c7164215d474f",
+    "url": "/solver2048/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "87d0736058996e67496a",
+    "url": "/solver2048/static/css/main.64ef8c6d.chunk.css"
+  },
+  {
+    "revision": "3159f02c7164215d474f",
+    "url": "/solver2048/static/js/2.0b9a94f9.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/solver2048/static/js/2.0b9a94f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87d0736058996e67496a",
+    "url": "/solver2048/static/js/main.16a3fcdc.chunk.js"
+  },
+  {
+    "revision": "471638c50b4b8b4331c1",
+    "url": "/solver2048/static/js/runtime-main.2930d1aa.js"
+  }
+]);
