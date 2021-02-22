629c5648ac8c0d7fcce9b75105a1827d223dc1e1
Daniele Bianchin <bianchin.daniele.b@gmail.com>
2021-02-22 05:30:16 +0100

    add GridLike, Transpose, Reverse traits default implementation

--- bin/game_ai/src/game.rs
+++ bin/game_ai/src/game.rs
@@ -43,8 +43,9 @@ pub enum PlayerMove {
 }
 
 /// `GameGrid` type containing the encoded grid state, defining the grid behavior.
-pub struct GameGrid {
-  encoded_state: EncodedGrid,
+pub struct GameGrid<T: GameGridState> {
+  state: T,
+  encoded: bool,
 }
 
 /// Contains the information regarding the encoded processing of the move for a single row in the grid.
@@ -69,21 +70,81 @@ pub struct MoveResult {
 /// Marker trait to define the set of types that a grid tile can assume
 trait EntryType {}
 
-/// Marker trait to define an object that is capable of exhibiting a grid-like behavior
-trait GridLike {}
+/// Marker trait to define a set of types for the state of `GameGrid`
+trait GameGridState {}
+
+/// Trait to define an object that is capable of exhibiting a grid-like behavior
+trait GridLike {
+  type Target: EntryType;
+
+  /// Gets a mutable reference to the `Grid<T>` underlying the GridLike object
+  fn get_grid_mut(&mut self) -> &mut Grid<Self::Target>;
+
+  /// Gets an immutable reference to the `Grid<T>` underlying the GridLike object
+  fn get_grid(&self) -> &Grid<Self::Target> {
+    // deref coercion to immutable
+    self.get_grid_mut()
+  }
+}
 
 /// Implements the transposition for method for `GridLike` structures
-trait Transpose<T: EntryType>: GridLike {
+trait Transpose: GridLike {
 
   /// Returns a `Copy` value of the transposed `GridLike` object
-  fn transpose(&mut self) -> &mut Self;
+  fn get_transpose(&self) -> Grid<Self::Target> {
+
+    // grid is Copy so can do
+    let grid = *self.get_grid();
+
+    let mut tmp;
+    for i in 0..GRID_SIDE {
+      for j in (i + 1)..GRID_SIDE {
+        tmp = grid[i][j];
+        grid[i][j] = grid[j][i];
+        grid[j][i] = tmp;
+      }
+    }
+
+    grid
+  }
+
+  /// Transposes the `GridLike` object in place
+  fn transpose(&mut self) -> &mut Self {
+    
+    *self.get_grid_mut() = self.get_transpose();
+
+    self
+  }
 }
 
 /// Implements the horizontal reverse of a `GridLike` structure
 trait Reverse: GridLike {
 
+  /// Return a copy of the horizontally reversed grid in the `GridLike` object
+  fn get_reverse(&self) -> Grid<Self::Target> {
+
+    // grid is Copy so can do
+    let grid = *self.get_grid();
+
+    let mut tmp;
+    for i in 0..GRID_SIDE {
+      for j in 0..(GRID_SIDE / 2) {
+        tmp = grid[i][j];
+        grid[i][j] = grid[i][GRID_SIDE - 1 - j];
+        grid[i][GRID_SIDE - 1 - j] = tmp;
+      }
+    }
+
+    grid
+  }
+
   /// Reverses the `GridLike` object in place horizontally and returns the mutable reference to itself for chaining
-  fn reverse<T: EntryType>(&self) -> Grid<T>;
+  fn reverse(&mut self) -> &mut Self {
+    
+    *self.get_grid_mut() = self.get_reverse();
+
+    self
+  }
 }
 
 
@@ -96,25 +157,37 @@ trait Reverse: GridLike {
 
 impl EntryType for GameGridPrimitive {}
 impl EntryType for DestGridPrimitive {}
+impl GameGridState for Grid<GameGridPrimitive> {}
+impl GameGridState for EncodedGrid {}
+
 
 // Grid
 
 impl<T: EntryType> GridLike for Grid<T> {}
+impl<T: EntryType> Transpose for Grid<T> {}
+impl<T: EntryType> Reverse for Grid<T> {}
+
 
 // GameGrid
 
-impl Clone for GameGrid {
+impl<T: GameGridState> Clone for GameGrid<T> {
   fn clone(&self) -> Self {
     *self
   }
 }
-impl Copy for GameGrid {}
+
+impl<T: GameGridState> Copy for GameGrid<T> {}
+
+impl<T: GameGridState> GridLike for GameGrid<T> {}
+impl<T: GameGridState> Transpose for GameGrid<T> {}
+impl<T: GameGridState> Reverse for GameGrid<T> {}
+
+
+//------------------------------------------------
+// Unit tests
+//------------------------------------------------
 
 #[cfg(test)]
 mod tests {
 
-
 }
-
-
-// Blanket
