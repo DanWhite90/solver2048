d2b182594df8b505cef58280f55aa01c298f2f8f
Daniele Bianchin <bianchin.daniele.b@gmail.com>
2021-03-02 04:46:32 +0100

    add heuristic functions and utility function plus tests

--- bin/game_ai/src/ai/core.rs
+++ bin/game_ai/src/ai/core.rs
@@ -23,6 +23,13 @@ pub const MOVE_CHILDREN_ARR_LENGTH: usize = MOVE_CHILDREN_ARR_LENGTH!(AVAILABLE_
 
 const LOG2_VICTORY_THRESHOLD: usize = 11; // need macro to make it log of VICTORY_THRESHOLD in game core.
 const TOT_MONOTONICITY_DIVISOR: usize = TOT_MONOTONICITY_DIVISOR!(GRID_SIDE);
+const GRID_NUM_GAP_SENSITIVITY: f64 = 0.8;
+
+// Utility Parameters const
+const ALPHA: f64 = 0.4; // Monotonicity weight
+const BETA: f64 = 0.2; // Emptiness weight
+const GAMMA: f64 = 0.15; // Mergeability weight
+const HOMOGENEITY_DEGREE: f64 = 8.; // Regulates the growth and concavity/convexity of the utility function
 
 
 // DATA STRUCTURES
@@ -184,7 +191,7 @@ fn decode_key(key: usize) -> (PlayerMove, EntryType, usize, usize) {
 fn heuristics_scores(grid: &Grid<EncodedGrid>) -> (f64, f64, f64, f64) {
   let (mut inc_h, mut inc_v, mut dec_h, mut dec_v) = (0, 0, 0, 0);
   let mut sequence_completeness = [0; LOG2_VICTORY_THRESHOLD];
-  let mut log_entry = 0;
+  let mut log_entry;
   let mut empty_tiles = 0;
   let mut max_tile = 0;
 
@@ -218,22 +225,32 @@ fn heuristics_scores(grid: &Grid<EncodedGrid>) -> (f64, f64, f64, f64) {
 
   let log_max = (max_tile as f64).log2() as usize;
   let clutter_penalty: f64 = if log_max > 1 {
-    sequence_completeness.iter().fold(0., |acc, value| acc + *value as f64)
+    sequence_completeness.iter().fold(0., |acc, value| acc + *value as f64) / (log_max * (log_max + 1) / 2) as f64
   } else {
     0.
   };
 
   ((cmp::max(inc_h, dec_h) + cmp::max(inc_v, dec_v) - TOT_MONOTONICITY_DIVISOR / 2) as f64 / TOT_MONOTONICITY_DIVISOR as f64 * 2., 
-    empty_tiles as f64 / (GRID_SIDE * GRID_SIDE - 1) as f64, 
-    1. - clutter_penalty * 0.8, 
+    empty_tiles as f64 / (GRID_SIDE * GRID_SIDE) as f64, 
+    1. - clutter_penalty * GRID_NUM_GAP_SENSITIVITY, 
     log_max as f64 / LOG2_VICTORY_THRESHOLD as f64,
   )
 }
 
 /// Computes the utility of a set of heuristics scores
 pub fn utility(grid: &Grid<EncodedGrid>) -> f64 {
+  let scores = heuristics_scores(grid);
 
-  0.
+  if scores.3 < 1. {
+    // Cobb-Douglas utility
+    scores.0.powf(HOMOGENEITY_DEGREE * ALPHA)
+    * scores.1.powf(HOMOGENEITY_DEGREE * BETA)
+    * scores.2.powf(HOMOGENEITY_DEGREE * GAMMA)
+    * scores.3.powf(HOMOGENEITY_DEGREE * (1. - ALPHA - BETA - GAMMA))
+  } else {
+    // Winning tile is present so return infinity
+    f64::INFINITY
+  }
 }
 
 //------------------------------------------------
@@ -283,6 +300,60 @@ mod tests {
     // |01|10|1|11| = 55
     assert_eq!(decode_key(55), (PlayerMove::Down, 4, 1, 2));
 
+  }
+
+
+  // Testing heuristics_scores()
+
+  #[test]
+  pub fn test_heuristics_scores_low() {
+    let grid = Grid::from_decoded(&[
+      [2, 4, 2, 4],
+      [4, 2, 4, 2],
+      [2, 4, 2, 4],
+      [4, 2, 4, 2],
+    ]);
+
+    let result = heuristics_scores(&grid);
+
+    assert_eq!(result.0, 0., "Monotonicity");
+    assert_eq!(result.1, 0., "Emptiness");
+    assert_eq!(result.2, 1. - 1. * GRID_NUM_GAP_SENSITIVITY, "Mergeability");
+    assert_eq!(result.3, 2. / LOG2_VICTORY_THRESHOLD as f64, "Maximum tile");
+  }
+
+  #[test]
+  pub fn test_heuristics_scores_empty() {
+    let grid = Grid::from_decoded(&[
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+    ]);
+
+    let result = heuristics_scores(&grid);
+
+    assert_eq!(result.0, 1., "Monotonicity");
+    assert_eq!(result.1, 1., "Emptiness");
+    assert_eq!(result.2, 1., "Mergeability");
+    assert_eq!(result.3, 0., "Maximum tile");
+  }
+
+  #[test]
+  pub fn test_heuristics_scores_high() {
+    let grid = Grid::from_decoded(&[
+      [8, 4, 2, 0],
+      [4, 2, 0, 0],
+      [2, 0, 0, 0],
+      [0, 0, 0, 0],
+    ]);
+
+    let result = heuristics_scores(&grid);
+
+    assert_eq!(result.0, 1., "Monotonicity");
+    assert_eq!(result.1, 10. / (GRID_SIDE * GRID_SIDE) as f64, "Emptiness");
+    assert_eq!(result.2, 1. - 1. * GRID_NUM_GAP_SENSITIVITY , "Mergeability");
+    assert_eq!(result.3, 3. / LOG2_VICTORY_THRESHOLD as f64, "Maximum tile");
   }
 
 }
