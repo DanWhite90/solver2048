20b8a3615e2f7c1d8c48cb772447f62f0abd5585
Daniele Bianchin <bianchin.daniele.b@gmail.com>
2021-04-08 17:46:19 +0200

    add routing to extended game app and css

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import {connect} from "react-redux";
+import {BrowserRouter as Router, Switch, Route, Link, Redirect} from "react-router-dom";
 
 import GameWrapper from "./game/GameWrapper";
 import GameStart from "./game/GameStart";
@@ -7,11 +8,28 @@ import {GAME_INIT} from "../globalOptions";
 
 const App = props => {
 
-  if (props.status === GAME_INIT) {
-    return <GameStart />;
-  } else {
-    return <GameWrapper />;
-  }
+  // if (props.status === GAME_INIT) {
+  //   return <GameStart />;
+  // } else {
+  //   return <GameWrapper />;
+  // }
+
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <GameStart />
+        </Route>
+        <Route path="/game" exact>
+          {props.status === GAME_INIT ? <Redirect to="/" /> : <GameWrapper />}
+        </Route>
+        <Route path="/scoreboard" exact>
+          {props.status === GAME_INIT ? <Redirect to="/" /> : null}
+        </Route>
+      </Switch>
+    </Router>
+  );
+
 }
 
 const mapStateToProps = state => {
--- src/components/game/GameStart.js
+++ src/components/game/GameStart.js
@@ -4,6 +4,7 @@ import {Container, Button} from "react-bootstrap";
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faFrown} from '@fortawesome/free-regular-svg-icons/faFrown';
 import {faGithub} from '@fortawesome/free-brands-svg-icons/faGithub';
+import {Link} from "react-router-dom";
 
 import * as actions from "../../actions";
 import {GAME_STARTED} from "../../globalOptions";
@@ -22,14 +23,14 @@ const GameStart = props => {
     <Container fluid className="wrapper">
       <h1>2048</h1>
       <h3><pre><code>{"<Solver />"}</code></pre></h3>
-      <Button 
-        variant="danger"
+      <Link
+        to="/game"
         onTouchStart={handleTouch}
         onClick={handleClick}
-        className="start rounded-circle"
+        id="start"
       >
         START
-      </Button>
+      </Link>
       <h5>An approximate fast AI for 2048</h5>
       <p><em>Disclaimer: You can't guarantee victory in a stochastic game <FontAwesomeIcon icon={faFrown} /></em></p>
       <p>For more information visit my <a href="https://github.com/DanWhite90/solver2048"><FontAwesomeIcon icon={faGithub} /> GitHub</a></p>
